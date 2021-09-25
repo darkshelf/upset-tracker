7066334515481bbaa75a34d8ccf3b5caf46d8218
Michael Foos <darkshelf@gmail.com>
2021-09-25 12:13:39 -0400

    Adding in node file to serve mock data to preserve number of AI calls made to pstmn;Initial rRefactoring of filter funcionality; General code cleanup

--- src/components/GameView.vue
+++ src/components/GameView.vue
@@ -1,14 +1,22 @@
 <template>
     <div>
         <h1>Game View</h1>
-        <div class="matchups">
+        <div>
+            <!-- Sort Buttons -->
             <button v-on:click="sortValueDesc()">Sort By Odds</button>            
-            <button v-on:click="sortValueAsc()">Sort by Value</button>
-            <ul>
-                <li v-for="(game, index) in games" :key="index" v-bind:class="{chosen: game.chosen}">
+            <button v-on:click="sortValueAsc()">Sort by Value</button>            
+            <button v-on:click="filters.push(filterAvailable)">Filter Available</button>   
+
+            <!-- Region Filtering needs to be better thought out within the displayGames() logic -->
+            <!-- <button v-for="(region, index) in regions" :key="index" v-on:click="displayRegion(region.regionId)">{{region.regionName}}</button> -->
+
+            <!-- List of each individual matchup -->
+            <ul class="matchups">
+                <li v-for="(game, index) in displayGames" :key="index" v-bind:class="{chosen: game.chosen}">
                     <IndividualMatchup :game="game" :isMyChoice="false" />
                 </li>                                
-            </ul>            
+            </ul>  
+
         </div>
     </div>
 </template>
@@ -23,47 +31,85 @@
         data() {
             return {
                 games: [],
-                regions: []
+                regions: [],
+                baseGames: [],
+                sort: (x) => x,
+                filters: []
             }
         }, 
         methods: {
-            extractGames(region) {
-                const regionName = region.regionName;
-                const games = region.games;
-
-                const gamesWithRegion = games.map(game => {
-                    return { ...game, regionName: regionName }
+            // Sorts the games by highest potential points value using seed differential
+            // TODO: Refactor out this.games mutation into a return function
+            sortValueAsc(){
+                this.games = this.baseGames.sort( (a, b) => (a.visitingTeam.seed - a.homeTeam.seed) - (b.visitingTeam.seed - b.homeTeam.seed) )
+            },
+            // Sorts the games by lowest potential points value using seed differential 
+            // TODO: Refactor out this.games mutation into a return function
+            sortValueDesc(){
+                this.games = this.baseGames.sort( (a, b) => (b.visitingTeam.seed - b.homeTeam.seed) - (a.visitingTeam.seed - a.homeTeam.seed) )
+            },
+            // Filters the games for a specified region
+            displayRegion(regionId){
+                return this.baseGames.filter( game => game.regionId === regionId )
+            },
+            // Filters the games by the ones already chosen
+            // TODO: Refactor out this.games mutation into a return function
+            filterChosen(games) {
+                return this.games.filter( game => game.chosen )
+            },
+            // Filters the games by the ones that available for selection
+            filterAvailable(games) {
+                return games.filter( game => !game.chosen)
+            }
+        }, 
+        computed: {        
+            // Currently broken, needs to be addressed    
+            displayGames: function() {
+                let gamesArray = this.baseGames;  
+                // Apply all filters from the filters array to gamesArray
+                this.filters.forEach(filter =>{
+                    console.log(filter);
+                    gamesArray = filter(gamesArray);
                 })
 
-                return gamesWithRegion;
-            },
-            sortValueAsc(){
-                this.games = this.games.sort( (a, b) => (a.visitingTeam.seed - a.homeTeam.seed) - (b.visitingTeam.seed - b.homeTeam.seed) )
-            },
-            // 
-            sortValueDesc(){
-                this.games = this.games.sort( (a, b) => (b.visitingTeam.seed - b.homeTeam.seed) - (a.visitingTeam.seed - a.homeTeam.seed) )
+                return gamesArray
             }
-        },        
+        },       
         mounted () {
 
-            const apiHost = 'https://d8b2602d-6dc9-4e4f-836e-bd5357c91710.mock.pstmn.io/games/schedule/week1';            
+            // Temp change to save on server requests to pstmn. Use local node server for Dev
+            // const apiHost = 'https://d8b2602d-6dc9-4e4f-836e-bd5357c91710.mock.pstmn.io/games/schedule/week1';            
+            const apiHost ='http://localhost:3000/week1';
 
             fetch(apiHost)
             .then(response => response.json())
             .then(data => {
                 this.regions = data;                
-                const newData = data.map(region => this.extractGames(region))
-            
+                const newData = data.map(region => {
+                    return region.games.map( game => {
+                        // Applies region information to each game object
+                        return {regionName: region.regionName, regionId: region.regionId, ...game}
+                    })
+                })
+
                 return newData;
             })
-            .then(gamesData => {                
+            // TODO: Combine this into previous .then block via concatAll
+            .then(gamesData => {    
+                const gamesArray = [];            
                 gamesData.forEach( region => {                    
                     region.forEach( (game) => {                        
-                        this.games.push(game)
+                        gamesArray.push(game)
                     })
-                })                
-            })
+                })
+
+                // Preserve a base array of all games in the result set
+                this.baseGames =  gamesArray;          
+                // Array of games that are displayed in the result set after sorts and filter.
+                // This can likely be refactored for a cleaner implementation       
+                this.games = this.baseGames;
+            }            
+            )
             .catch((err) => {
                 console.error(err);
             });
@@ -73,7 +119,7 @@
 </script>
 
 <style scoped>
-    ul {
+    ul.matchups {
         display: flex;
         flex-flow: wrap;
         justify-content: space-evenly;
